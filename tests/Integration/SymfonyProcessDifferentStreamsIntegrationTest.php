3fcfd4bf04917b48d476f04b07654bee1cfbc52a
riki137 <richard@popelis.sk>
2025-05-27 10:27:48 +0200

    more SymfonyProcess tests

---
+++ tests/Integration/SymfonyProcessDifferentStreamsIntegrationTest.php
@@ -0,0 +1,99 @@
+<?php
+declare(strict_types=1);
+
+namespace PhpStreamIpc\Tests\Integration;
+
+use PHPUnit\Framework\TestCase;
+use PhpStreamIpc\IpcPeer;
+use PhpStreamIpc\Message\LogMessage;
+use PhpStreamIpc\Message\Message;
+use Symfony\Component\Process\Process;
+
+final class SymfonyProcessDifferentStreamsIntegrationTest extends TestCase
+{
+    private string $stdoutScript;
+    private string $stderrScript;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $autoloadPath = realpath(__DIR__ . '/../../vendor/autoload.php');
+        if (!$autoloadPath) {
+            $this->markTestSkipped('Could not locate vendor/autoload.php');
+        }
+
+        $this->stdoutScript = sys_get_temp_dir() . '/ipc_stdout_echo_' . uniqid() . '.php';
+        $stdout = <<<'PHP'
+<?php
+declare(strict_types=1);
+
+require %s;
+
+use PhpStreamIpc\IpcPeer;
+use PhpStreamIpc\Message\Message;
+
+$peer    = new IpcPeer();
+$session = $peer->createStdioSession();
+$session->onRequest(fn(Message $msg) => $msg);
+$peer->tick();
+PHP;
+        file_put_contents(
+            $this->stdoutScript,
+            sprintf($stdout, var_export($autoloadPath, true))
+        );
+
+        $this->stderrScript = sys_get_temp_dir() . '/ipc_stderr_echo_' . uniqid() . '.php';
+        $stderr = <<<'PHP'
+<?php
+declare(strict_types=1);
+
+require %s;
+
+use PhpStreamIpc\IpcPeer;
+use PhpStreamIpc\Message\Message;
+
+$peer    = new IpcPeer();
+$session = $peer->createStreamSession(STDERR, STDIN);
+$session->onRequest(fn(Message $msg) => $msg);
+$peer->tick();
+PHP;
+        file_put_contents(
+            $this->stderrScript,
+            sprintf($stderr, var_export($autoloadPath, true))
+        );
+    }
+
+    protected function tearDown(): void
+    {
+        @unlink($this->stdoutScript);
+        @unlink($this->stderrScript);
+        parent::tearDown();
+    }
+
+    public function testResponsesFromDifferentStreams(): void
+    {
+        $process1 = new Process([PHP_BINARY, $this->stdoutScript]);
+        $process2 = new Process([PHP_BINARY, $this->stderrScript]);
+
+        $peer     = new IpcPeer();
+        $session1 = $peer->createSymfonyProcessSession($process1);
+        $session2 = $peer->createSymfonyProcessSession($process2);
+
+        $pending1 = $session1->request(new LogMessage('from-stdout', 'info'), 1.0);
+        $pending2 = $session2->request(new LogMessage('from-stderr', 'info'), 1.0);
+
+        $peer->tickFor(1.0);
+
+        $resp1 = $pending1->await();
+        $resp2 = $pending2->await();
+
+        $this->assertInstanceOf(LogMessage::class, $resp1);
+        $this->assertSame('from-stdout', $resp1->message);
+        $this->assertInstanceOf(LogMessage::class, $resp2);
+        $this->assertSame('from-stderr', $resp2->message);
+
+        $process1->stop();
+        $process2->stop();
+    }
+}
---
+++ tests/Integration/SymfonyProcessMultiProcessNotificationIntegrationTest.php
@@ -0,0 +1,107 @@
+<?php
+declare(strict_types=1);
+
+namespace PhpStreamIpc\Tests\Integration;
+
+use PHPUnit\Framework\TestCase;
+use PhpStreamIpc\IpcPeer;
+use PhpStreamIpc\Message\LogMessage;
+use PhpStreamIpc\Message\Message;
+use Symfony\Component\Process\Process;
+
+final class SymfonyProcessMultiProcessNotificationIntegrationTest extends TestCase
+{
+    private string $scriptPath1;
+    private string $scriptPath2;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $autoloadPath = realpath(__DIR__ . '/../../vendor/autoload.php');
+        if (!$autoloadPath) {
+            $this->markTestSkipped('Could not locate vendor/autoload.php');
+        }
+
+        $this->scriptPath1 = sys_get_temp_dir() . '/ipc_multi_notify_p1_' . uniqid() . '.php';
+        $script1 = <<<'PHP'
+<?php
+declare(strict_types=1);
+
+require %s;
+
+use PhpStreamIpc\IpcPeer;
+use PhpStreamIpc\Message\LogMessage;
+
+$peer    = new IpcPeer();
+$session = $peer->createStdioSession();
+
+$session->notify(new LogMessage('p1_1', 'info'));
+usleep(100_000);
+$session->notify(new LogMessage('p1_2', 'info'));
+PHP;
+        file_put_contents(
+            $this->scriptPath1,
+            sprintf($script1, var_export($autoloadPath, true))
+        );
+
+        $this->scriptPath2 = sys_get_temp_dir() . '/ipc_multi_notify_p2_' . uniqid() . '.php';
+        $script2 = <<<'PHP'
+<?php
+declare(strict_types=1);
+
+require %s;
+
+use PhpStreamIpc\IpcPeer;
+use PhpStreamIpc\Message\LogMessage;
+
+$peer    = new IpcPeer();
+$session = $peer->createStdioSession();
+
+usleep(50_000);
+$session->notify(new LogMessage('p2_1', 'info'));
+usleep(100_000);
+$session->notify(new LogMessage('p2_2', 'info'));
+PHP;
+        file_put_contents(
+            $this->scriptPath2,
+            sprintf($script2, var_export($autoloadPath, true))
+        );
+    }
+
+    protected function tearDown(): void
+    {
+        @unlink($this->scriptPath1);
+        @unlink($this->scriptPath2);
+        parent::tearDown();
+    }
+
+    public function testMultipleSymfonyProcessesNotificationsOrder(): void
+    {
+        $process1 = new Process([PHP_BINARY, $this->scriptPath1]);
+        $process2 = new Process([PHP_BINARY, $this->scriptPath2]);
+
+        $peer     = new IpcPeer();
+        $session1 = $peer->createSymfonyProcessSession($process1);
+        $session2 = $peer->createSymfonyProcessSession($process2);
+
+        $received = [];
+        $handler = function (Message $msg) use (&$received): void {
+            $this->assertInstanceOf(LogMessage::class, $msg);
+            $received[] = $msg->message;
+        };
+
+        $session1->onMessage($handler);
+        $session2->onMessage($handler);
+
+        $start = microtime(true);
+        while (count($received) < 4 && (microtime(true) - $start) < 1.0) {
+            $peer->tick(0.1);
+        }
+
+        $process1->stop();
+        $process2->stop();
+
+        $this->assertSame(['p1_1', 'p2_1', 'p1_2', 'p2_2'], $received);
+    }
+}
