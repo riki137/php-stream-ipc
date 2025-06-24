0da7b609645ba7714b20440369da83d91e785c81
Richard Popeliš <richard@popelis.sk>
2025-06-24 14:59:04 +0200

    add exception handling support to IpcSession (#4)

--- tests/Unit/NativeIpcPeerInvalidStreamTest.php
+++ tests/Unit/NativeIpcPeerInvalidStreamTest.php
@@ -7,20 +7,27 @@ use StreamIpc\NativeIpcPeer;
 
 final class NativeIpcPeerInvalidStreamTest extends TestCase
 {
-    public function testTickThrowsInvalidStreamExceptionWithSession(): void
+    public function testTickTriggersExceptionHandlerAndRemovesSession(): void
     {
         [$a, $b] = stream_socket_pair(STREAM_PF_UNIX, STREAM_SOCK_STREAM, 0);
         $peer = new NativeIpcPeer();
         $session = $peer->createStreamSession($a, $a, $b);
         fclose($b);
 
-        try {
-            $peer->tick();
-            $this->fail('No exception thrown');
-        } catch (InvalidStreamException $e) {
-            $this->assertSame($session, $e->getSession());
-        } finally {
-            fclose($a);
-        }
+        $called = false;
+        $session->onException(function (InvalidStreamException $e) use (&$called, $session) {
+            $called = true;
+            \PHPUnit\Framework\Assert::assertSame($session, $e->getSession());
+        });
+
+        $peer->tick();
+
+        $this->assertTrue($called);
+
+        $prop = new \ReflectionProperty($peer, 'sessions');
+        $prop->setAccessible(true);
+        $this->assertSame([], $prop->getValue($peer));
+
+        fclose($a);
     }
 }
